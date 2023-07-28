7a6ceec13c67a9bd946f48119d606e4f36b78a93
habibellah <habibayata1@gmail.com>
2023-07-28 15:45:01 +0200

    Add Movie Details Screen Ui

--- app/src/main/java/com/example/moviesapp/home_feature/presentation/screens/movieDetailsScreen/MovieDetailsScreen.kt
+++ app/src/main/java/com/example/moviesapp/home_feature/presentation/screens/movieDetailsScreen/MovieDetailsScreen.kt
@@ -3,10 +3,12 @@ package com.example.moviesapp.home_feature.presentation.screens.movieDetailsScre
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MovieDetailsScreen(){
@@ -15,7 +17,8 @@ fun MovieDetailsScreen(){
 
 @Composable
 private fun MovieDetailsContent() {
-   Box(modifier = Modifier.fillMaxSize()){
+   Box(modifier = Modifier.fillMaxSize()
+      .padding(16.dp)){
       Column (modifier = Modifier
          .fillMaxSize()
          .verticalScroll(rememberScrollState())){
